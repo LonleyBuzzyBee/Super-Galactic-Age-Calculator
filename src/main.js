9d4fc18a4f03b35ac41ed51dfe64c8235b7b8565
mai cook <mailiecook@yahoo.com>
2020-04-17 13:42:52 -0700

    added variables name, age, planet, favM, and user in main.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,18 @@
 import $ from 'jquery';
 import './../src/style.css';
 
+$(document).ready(function(){
+  $("#calc").submit(function(event) {
+    event.preventDefault();
+    let name  = $("#name").val();
+    let age = parseInt($("#age").val());
+    let planet = $("#planet").val();
+    let favM = $("#favM").val();
+    let user = new User(name,age,planet,favM);
+    user.sortP();
+    console.log(user);
+
+  });
+});
+
+
