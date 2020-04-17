40f406ae3d401c5101b2a5f50be336b56c7a9239
mai cook <mailiecook@yahoo.com>
2020-04-17 10:27:03 -0700

    added sortP function, test 2 failed again bc functions to be thrown into mars,venus,mercury,jupiter don't exist yet. next step creat functions

--- src/tdd.js
+++ src/tdd.js
@@ -1,3 +1,5 @@
+import { userInfo } from "os";
+
 //business logic
 export class User {
   constructor (name, age, planet, favM) {
@@ -6,4 +8,23 @@ export class User {
     this.planet = planet;
     this.favM = favM;
   }
+  sortP() {
+    switch(this.planet){
+      case "mars":
+        this.mars();
+        break;
+      case "jupiter":
+        this.jupiter();
+        break;
+      case "venus":
+        this.venus();
+        break;
+      case "mercury":
+        this.mercury();
+        break;
+      default:
+        console.log("error choose a planet");
+    }
+  }
 }
+
